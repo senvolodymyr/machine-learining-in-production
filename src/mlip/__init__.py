db2763f905233541723a54427f7ffcd9720aec7e
Volodymyr Sen <volodymyr.sen@gmail.com>
2025-04-06 10:08:28 +0200

    Adds dummy commit to test workflow

--- src/mlip/__init__.py
+++ src/mlip/__init__.py
@@ -1,2 +1,6 @@
 def get_project_name() -> str:
+<<<<<<< HEAD
     return "Machine Learning in Production"
+=======
+    return "Machine Learning in Production Basic Setup"
+>>>>>>> e0ba812 (Adds dummy commit to test workflow)
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,8 @@ from mlip import get_project_name
 
 
 def test_get_project_name():
+<<<<<<< HEAD
     assert get_project_name() == "Machine Learning in Production"
+=======
+    assert get_project_name() == "Machine Learning in Production Basic Setup"
+>>>>>>> e0ba812 (Adds dummy commit to test workflow)
