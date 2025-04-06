0579795286488626433d6e8ba76f692edffde045
Volodymyr Sen <volodymyr.sen@gmail.com>
2025-04-06 12:20:39 +0200

    Fixes text files after rebase

--- src/mlip/__init__.py
+++ src/mlip/__init__.py
@@ -1,6 +1,2 @@
 def get_project_name() -> str:
-<<<<<<< HEAD
-    return "Machine Learning in Production"
-=======
     return "Machine Learning in Production Basic Setup"
->>>>>>> e0ba812 (Adds dummy commit to test workflow)
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,4 @@ from mlip import get_project_name
 
 
 def test_get_project_name():
-<<<<<<< HEAD
-    assert get_project_name() == "Machine Learning in Production"
-=======
     assert get_project_name() == "Machine Learning in Production Basic Setup"
->>>>>>> e0ba812 (Adds dummy commit to test workflow)
